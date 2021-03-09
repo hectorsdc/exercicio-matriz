6f281ad6ad0a723b76f58f11edd0c0a47b4cfbda
Hector Soares <hector-ce@hotmail.com>
2021-03-08 22:57:40 -0300

    Variáveis alteradas para o padrão C#

--- Course/Program.cs
+++ Course/Program.cs
@@ -7,36 +7,36 @@ namespace Course {
         static void Main(string[] args) {
             string[] s = Console.ReadLine().Split(' ');
 
-            int M = int.Parse(s[0]);
-            int N = int.Parse(s[1]);
+            int m = int.Parse(s[0]);
+            int n = int.Parse(s[1]);
 
-            int[,] mat = new int[M, N];
+            int[,] mat = new int[m, n];
 
-            for (int i = 0; i < M; i++) {
+            for (int i = 0; i < m; i++) {
                 s = Console.ReadLine().Split(' ');
-                for (int j = 0; j < N; j++) {
+                for (int j = 0; j < n; j++) {
                     mat[i, j] = int.Parse(s[j]);
                 }
             }
 
-            int X = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
 
 
 
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (mat[i, j] == X) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i, j] == x) {
                         Console.WriteLine("Position " + i + "," + j + ":");
                         if ((j - 1) >= 0) {
                             Console.WriteLine("Left: " + mat[i, j - 1]);
                         }
-                        if ((j + 1) <= (N - 1)) {
+                        if ((j + 1) <= (n - 1)) {
                             Console.WriteLine("Right: " + mat[i, j + 1]);
                         }
                         if ((i - 1) >= 0) {
                             Console.WriteLine("Up: " + mat[i - 1, j]);
                         }
-                        if ((i + 1) <= (M - 1)) {
+                        if ((i + 1) <= (m - 1)) {
                             Console.WriteLine("Down: " + mat[i + 1, j]);
                         }
                     }
